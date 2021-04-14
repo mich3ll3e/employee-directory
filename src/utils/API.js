157ecccaa6fc6f7c8d0a9c7da4be615713b32f59
mich3ll3e <michelle.ouyang@outlook.com>
2021-04-14 15:36:44 -0400

    implemented api calls to get directory

---
+++ src/utils/API.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getEmployees: () => axios.get("https://randomuser.me/api/?results=200&nat=us")
+};
